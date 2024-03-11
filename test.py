6c301d9261740630949053cb83de71095cc1a2bc
TzahiAn <tzahi-gotech@stigg.io>
2024-03-11 13:39:36 +0200

    add headless mode

--- test.py
+++ test.py
@@ -8,10 +8,13 @@ import time
 
 class MyTest(unittest.TestCase):
     def setUp(self):
-
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         self.SEARCH_INPUT = '//input[@id="search"]'
         self.url = "https://www.youtube.com/"
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(options=options)
         self.driver.maximize_window()
 
     def tearDown(self):
