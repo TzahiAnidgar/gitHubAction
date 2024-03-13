cb18f15429c47a501b709d0de0fe59c543394b07
TzahiAn <tzahi-gotech@stigg.io>
2024-03-13 11:34:49 +0200

    remove chrome driver

--- test.py
+++ test.py
@@ -10,14 +10,13 @@ import time
 
 class MyTest(unittest.TestCase):
     def setUp(self):
-        webdriver_service = Service('/Users/tzahianidgar/Documents/Project/automation-python/chromedriver')
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         self.SEARCH_INPUT = '//input[@id="search"]'
         self.url = "https://www.youtube.com/"
-        self.driver = webdriver.Chrome(service=webdriver_service,options=options)
+        self.driver = webdriver.Chrome(options=options)
         self.driver.maximize_window()
 
     def tearDown(self):
